c7eaceda9c8c9016c76fb13bcc5653865fea4086
Jude Boachie <jdboachie@gmail.com>
2025-08-22 23:39:08 +0000

    feat: add support for departments, restructure

--- src/middleware/validateAdminSchema.ts
+++ src/middleware/validateAdminSchema.ts
@@ -8,6 +8,6 @@ const loginSchemaForAdmin = Joi.object({
 
 export const validateAdminLoginAndSignUpSchema = (req: Request, res: Response, next: NextFunction) => {
 	const { error } = loginSchemaForAdmin.validate(req.body);
-	if (error) return res.status(400).json({ msg: "Invalid payload in the request parameters" });
+	if (error) return res.status(400).json({ msg: "Admin signup: Invalid payload in the request parameters" });
 	next();
 };
