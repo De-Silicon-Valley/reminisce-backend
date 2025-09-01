af9b741fadcdc7985f76bf6220ddccfb74e94336
Sharonmee <enamyawlui@gmail.com>
2025-09-01 02:08:24 +0000

    Update start script in package.json and enable synchronization in dataSource.ts

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -6,7 +6,7 @@ export const dataSource = new DataSource({
 	url: process.env.DB_URL,
 	// useUnifiedTopology: true,
 	// useNewUrlParser: true,
-	synchronize: false,
+	synchronize: true,
 	logging: true,
 	entities: [path.join(__dirname, "models", "*.{ts,js}")],
 	// port: Number(process.env.DB_PORT),
